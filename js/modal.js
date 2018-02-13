3ec98b967381a5b2671602beb08211df19b9f877
MixX0 <31200944+MixX0@users.noreply.github.com>
2018-02-13 14:15:26 +0300

    Проблема с драг энд дропом

--- js/modal.js
+++ js/modal.js
@@ -4,6 +4,7 @@
   var popupOpen = document.querySelector('.setup-open');
   var popupClose = window.util.setup.querySelector('.setup-close');
   var userNameInput = window.util.setup.querySelector('.setup-user-name');
+  var dialogHandle = document.querySelector('.upload');
 
   var onPopupOpenEscPress = function (evt) {
     window.util.isEscPressEvent(evt, onPopupCloseClick);
@@ -43,4 +44,52 @@
   popupClose.addEventListener('keydown', onPopupCloseEnterPress);
   popupOpen.addEventListener('click', onPopupOpenClick);
   popupOpen.addEventListener('keydown', onPopupOpenEnterPress);
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var flag = false;
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      flag = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      window.util.setup.style.top = (window.util.setup.offsetTop - shift.y) + 'px';
+      window.util.setup.style.left = (window.util.setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      if (flag) {
+        var onClickPreventDefault = function (prevDefEvt) {
+          prevDefEvt.preventDefault();
+          dialogHandle.removeEventListener('click', onClickPreventDefault);
+        };
+      }
+
+      dialogHandle.addEventListener('click', onClickPreventDefault);
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 })();
