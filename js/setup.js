e0e80062be5965f2f6a6ba658bf7c98120a7626d
Andrey <andrey_9112@mail.ru>
2018-04-30 17:12:42 +0300

    Доработал драг энд дроп

--- js/setup.js
+++ js/setup.js
@@ -2,9 +2,6 @@
 
 (function () {
   var templateWizard = document.querySelector('#similar-wizard-template').content;
-  var shopElement = document.querySelector('.setup-artifacts-shop');
-  var artifactsElement = document.querySelector('.setup-artifacts');
-  var draggedItem = null;
 
   var viewArtifacts = function (artifact) {
     var wizardElement = document.createElement('li');
@@ -26,47 +23,326 @@
   };
 
   // ПЕРЕТАСКИВАНИЕ
-  shopElement.addEventListener('dragstart', function (evt) {
-    draggedItem = null;
+  var DropArtifacts = function (options) {
+    var self = this;
 
-    if (evt.target.tagName === 'IMG') {
-      draggedItem = evt.target.cloneNode(true);
+    this.elem = options.elem;
+    this.artifactsContainer = this.elem.querySelector('.setup-artifacts');
+    this.cellInContainer = this.artifactsContainer.querySelectorAll('.setup-artifacts-cell');
+    this.dragObject = {};
+
+    this.onMouseDown = function (evt) {
+      evt.preventDefault();
+
+      if (evt.which !== 1) {
+        return null;
+      }
+
+      var elem = evt.target.closest('.shop-img');
+
+      if (!elem) {
+        return null;
+      }
+
+      // Начальные данные
+      self.dragObject.elem = elem;
+      self.dragObject.x = evt.pageX;
+      self.dragObject.y = evt.pageY;
+      self.dragObject.width = elem.offsetWidth;
+      self.dragObject.height = elem.offsetHeight;
+      self.dragObject.left = elem.getBoundingClientRect().left + window.pageXOffset;
+      self.dragObject.top = elem.getBoundingClientRect().top + window.pageYOffset;
+
+      document.addEventListener('mousemove', this.onMouseMove.bind(this));
+      document.addEventListener('mouseup', this.onMouseUp.bind(this));
+
+      return false;
+    };
+
+    this.onMouseMove = function (evt) {
+      evt.preventDefault();
+
+      if (!self.dragObject.elem) {
+        return null;
+      }
+
+      if (!self.dragObject.avatar) {
+        var shiftX = evt.pageX - self.dragObject.x;
+        var shiftY = evt.pageY - self.dragObject.y;
+
+        // Если сдвиг менее 3пх, то ничего не делаем
+        if (Math.abs(shiftX) < 3 && Math.abs(shiftY) < 3) {
+          return null;
+        }
+
+        this.dragObject.avatar = self.createAvatar();
+
+        // Если попали мимо аватара
+        if (!self.dragObject.avatar) {
+          self.dragObject = {};
+
+          return null;
+        }
+
+        // Центр захвата
+        self.dragObject.shiftX = evt.clientX - self.dragObject.elem.getBoundingClientRect().left;
+        self.dragObject.shiftY = evt.clientY - self.dragObject.elem.getBoundingClientRect().top;
+
+        self.startDrag(evt);
+
+        // Скрываем главный элемент, дальше работаем с аватаром
+        self.dragObject.elem.parentNode.removeChild(self.dragObject.elem);
+        // self.dragObject.elem.style.display = 'none';
+        // self.dragObject.elem.style.overflow = 'hidden';
+      }
+
+      // Разница от центра захвата
+      var newX = evt.clientX - self.dragObject.shiftX + pageXOffset;
+      var newY = evt.clientY - self.dragObject.shiftY + pageYOffset;
+      var scrollY;
+      var scrollX;
+
+      // Скролл при перемещении за границы окна
+      if (newY + self.dragObject.avatar.offsetHeight > document.documentElement.clientHeight) {
+        scrollY = newY + self.dragObject.avatar.offsetHeight - document.documentElement.clientHeight;
+
+        if (scrollY < 0) {
+          scrollY = 0;
+        }
+
+        scrollY = Math.min(scrollY, 10);
+        window.scrollBy(0, scrollY);
+        newY = Math.min(newY, document.documentElement.clientHeight - self.dragObject.avatar.offsetHeight);
+      }
+
+      if (newY - window.pageYOffset < 0) {
+        scrollY = Math.min(-newY + window.pageYOffset, 10);
+        newY = window.pageYOffset;
+
+        if (scrollY < 0) {
+          scrollY = 0;
+        }
+
+        window.scrollBy(0, -scrollY);
+        newY = Math.max(newY, 0);
+      }
+
+      if (newX - window.pageXOffset < 0) {
+        scrollX = Math.min(-newX + window.pageXOffset, 10);
+        newX = window.pageXOffset;
+
+        if (scrollX < 0) {
+          scrollX = 0;
+        }
+
+        window.scrollBy(-scrollX, 0);
+        newX = Math.max(newX, 0);
+      }
+
+      if (newX + self.dragObject.avatar.offsetWidth > document.documentElement.clientWidth) {
+        scrollX = newX + self.dragObject.avatar.offsetWidth - document.documentElement.clientWidth;
+
+        if (scrollX < 0) {
+          scrollX = 0;
+        }
+
+        scrollX = Math.min(scrollX, 10);
+        window.scrollBy(scrollX, 0);
+        newX = Math.min(newX, document.documentElement.clientWidth - self.dragObject.avatar.offsetWidth);
+
+      }
+
+      self.dragObject.avatar.style.left = newX + 'px';
+      self.dragObject.avatar.style.top = newY + 'px';
+
+      // Стили подсветки
+      this.addStyle(evt);
+
+      return false;
+    };
+
+    this.onMouseUp = function (evt) {
+      // Выравниваем аватар относительно контейнера, так как его координаты были относительно боди
+      if (self.dragObject.avatar) {
+        self.dragObject.avatar.style.top = evt.clientY - (this.elem.getBoundingClientRect().top - this.dragObject.elem.offsetHeight) - 35 + 'px';
+        self.dragObject.avatar.style.left = evt.clientX - (this.elem.getBoundingClientRect().left - this.dragObject.elem.offsetWidth) - 35 + 'px';
+        self.finishDrag(evt);
+      }
+
+      if (evt.target.parentNode === document) {
+        return null;
+      }
+
+      if (self.dragObject.elem) {
+        self.dragObject = {};
+      }
+
+      document.removeEventListener('mousemove', self.onMouseMove.bind(self));
+      document.removeEventListener('mouseup', self.onMouseUp.bind(self));
+    };
+  };
+
+  DropArtifacts.prototype.findDroppable = function (evt) {
+    this.dragObject.avatar.style.display = 'none';
+
+    var elem = document.elementFromPoint(evt.clientX, evt.clientY);
+
+    this.dragObject.avatar.style.display = 'block';
+
+    if (elem === null) {
+      return null;
     }
-  });
 
-  artifactsElement.addEventListener('dragover', function (evt) {
-    evt.preventDefault();
+    return elem.closest('.setup-artifacts .setup-artifacts-cell');
+  };
 
-    return false;
-  });
+  DropArtifacts.prototype.createAvatar = function () {
+    var avatar = this.dragObject.elem.cloneNode(true);
+    var elem = this.dragObject.elem;
 
-  artifactsElement.addEventListener('drop', function (evt) {
-    evt.preventDefault();
+    var old = {
+      parent: elem.parentNode,
+      nextSibling: elem.nextElementSibling,
+      position: elem.position || '',
+      left: elem.getBoundingClientRect().left - this.elem.getBoundingClientRect().left,
+      top: elem.getBoundingClientRect().top - this.elem.getBoundingClientRect().top,
+      zIndex: elem.zIndex || ''
+    };
 
-    if (evt.target.tagName !== 'IMG' && evt.target.children.length === 0) {
-      evt.target.classList.remove('dragenter-border');
-      evt.target.appendChild(draggedItem);
+    avatar.rollback = function () {
+      old.parent.insertBefore(avatar, old.nextSibling);
+      avatar.style.transition = '1s ease';
+      avatar.style.position = 'absolute';
+      avatar.style.zIndex = '9999';
+      avatar.classList.remove('avatar');
+
+      var currentX = parseFloat(avatar.style.left) + window.pageXOffset;
+      var currentY = parseFloat(avatar.style.top) + window.pageYOffset;
+
+      avatar.style.left = currentX + 'px';
+      avatar.style.top = currentY + 'px';
+
+      var left = old.left;
+      var top = old.top;
+      var moveX = currentX - old.left;
+      var moveY = currentY - old.top;
+      var max = Math.max(Math.abs(moveX), Math.abs(moveY));
+      var min = Math.min(Math.abs(moveX), Math.abs(moveY));
+      var steps = Math.floor(max / 10);
+      var minStep = min / steps;
+
+      if(Math.abs(moveX) > Math.abs(moveY) ) {
+        var long = 'left';
+        var short = 'top';
+      }
+      else {
+        var long = 'top';
+        var short = 'left';
+      }
+
+      if(long === 'left') {
+        var stepX = moveX > 0 ? -10 : 10;
+        var stepY = moveY > 0 ? -minStep : minStep;
+      }
+      else {
+        var stepY = moveY > 0 ? -10 : 10;
+        var stepX = moveX > 0 ? -minStep : minStep;
+      }
+
+      var timer;
+      var moveBack = function me() {
+        if(max > 10) {
+          avatar.style.left = parseFloat(avatar.style.left) + stepX + 'px';
+          avatar.style.top = parseFloat(avatar.style.top) + stepY + 'px';
+          max -= 10;
+          min -= minStep;
+          timer = setTimeout(me,10);
+        }
+        else {
+          avatar.style.left = left + 'px';
+          avatar.style.top = top + 'px';
+          max = 0;
+          timer = null;
+        }
+      }();
+    };
+
+    return avatar;
+  };
+
+  DropArtifacts.prototype.startDrag = function () {
+    var avatar = this.dragObject.avatar;
+
+    avatar.classList.add('avatar');
+    document.body.appendChild(avatar);
+  };
+
+  DropArtifacts.prototype.finishDrag = function (evt) {
+    var dropElement = this.findDroppable(evt);
+
+    if (!dropElement || dropElement.children.length !== 0) {
+      this.dragCancel();
+
+    } else {
+      this.dragEnd(dropElement, this.dragObject.avatar);
     }
-  });
+  };
 
-  artifactsElement.addEventListener('dragenter', function (evt) {
-    evt.preventDefault();
+  DropArtifacts.prototype.dragEnd = function (dragObj, dropElem) {
+    var avatar = this.dragObject.avatar;
 
-    if (evt.target.tagName === 'DIV' && evt.target.children.length === 0) {
-      evt.target.classList.add('dragenter-border');
+    avatar.classList.remove('avatar');
+    dragObj.appendChild(dropElem);
+
+    avatar.style.position = 'static';
+
+    // Удаляем ненужные стили с ячеек
+    if (avatar.parentNode.classList.contains('dragenter-border')) {
+      avatar.parentNode.className = 'setup-artifacts-cell dropzone';
     }
 
-    if (evt.target.tagName === 'IMG') {
-      evt.target.classList.add('dragenter-bg-color');
+    self.dragObject = {};
+  };
+
+  DropArtifacts.prototype.dragCancel = function () {
+    this.dragObject.avatar.rollback();
+  };
+
+  DropArtifacts.prototype.addStyle = function (evt) {
+    [].forEach.call(this.cellInContainer, function (it) {
+      it.className = 'setup-artifacts-cell dropzone';
+    });
+
+    this.dragObject.avatar.style.display = 'none';
+
+    var elem = document.elementFromPoint(evt.clientX, evt.clientY);
+
+    this.dragObject.avatar.style.display = 'block';
+
+    if (!elem) {
+      return null;
     }
+
+    if (elem.className === 'setup-artifacts-cell dropzone') {
+      elem.className = 'setup-artifacts-cell dropzone dragenter-bg-color dragenter-border';
+    }
+  };
+
+  DropArtifacts.prototype.init = function () {
+    var self = this;
+
+    this.elem.addEventListener('mousedown', function (evt) {
+      if (evt.target.closest('.shop-img')) {
+        self.onMouseDown(evt);
+      }
+    });
+  };
+
+  var dropArtifacts = new DropArtifacts({
+    elem: document.querySelector('.setup-artifacts-container')
   });
 
-  artifactsElement.addEventListener('dragleave', function (evt) {
-    evt.preventDefault();
-
-    evt.target.classList.remove('dragenter-border');
-    evt.target.classList.remove('dragenter-bg-color');
-  });
+  dropArtifacts.init();
 
   window.setup = {
     getRenderWizard: getRenderWizard
